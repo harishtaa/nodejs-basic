2bc2e0c6c2b380fc6f4338928fe5403ae6828772
Harish Adityawan <haradityawan@gmail.com>
2023-02-28 07:48:26 +0700

    add events-listener

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-const moment = require("moment/moment");
-const date = moment().format("M Do YY");
+const { EventEmitter } = require('events')
 
-console.log(date)
+const myEventEmitter = new EventEmitter();
+
+const makeCoffee = ({ name }) => {
+    console.log(`kopi ${name} berhasil dibuat`)
+}
+
+const makeBill = ({ price }) => {
+    console.log(`Bill sebesar ${price} telah dibuat!`)
+}
+
+// myEventEmitter.on('coffee-order', makeCoffee)
+// myEventEmitter.on('coffee-order', makeBill)
+
+const onCoffeeOrderedListener = ({ name, price }) => {
+    makeCoffee(name);
+    makeBill(price)
+}
+
+myEventEmitter.on('coffee-order', onCoffeeOrderedListener)
+
+myEventEmitter.emit('coffee-order', {name: 'Tubruk', price: 100000})
